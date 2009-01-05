5356209b631cbf3d632f37acbf9abba725065f01
cpedia <cpedia@23721618-7e43-11dd-88bd-cff828a63736>
2009-01-05 22:04:52 +0000

    initial set up the checklist project.

---
+++ trunk/chec.kli.st/checklist_web/config.py
@@ -0,0 +1,8 @@
+import os
+import logging
+
+APP_ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+# If we're debugging, turn the cache off, etc.
+# Set to true if we want to have our webapp print stack traces, etc
+DEBUG = os.environ['SERVER_SOFTWARE'].startswith('Dev')
+logging.info("Starting application in DEBUG mode: %s", DEBUG)
