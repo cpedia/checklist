ba6bd1f301e5777013f4b547caf6e890dec8a79e
cpedia <cpedia@23721618-7e43-11dd-88bd-cff828a63736>
2009-04-15 11:21:17 +0000

    quick checklist.

--- trunk/chec.kli.st/checklist_web/static/jscripts/checklist.js
+++ trunk/chec.kli.st/checklist_web/static/jscripts/checklist.js
@@ -2,6 +2,7 @@ YAHOO.namespace("checklist");
 YAHOO.namespace("checklist.columns");
 YAHOO.namespace("checklist.templates");
 YAHOO.namespace("checklist.templates.columns");
+YAHOO.namespace("checklist.items");
 
 String.prototype.trim = function () {
     return this.replace(/^[\s\,]*/, "").replace(/[\s\,]*$/, ""); //for remove the space and comma at the begining/end of the tag.
