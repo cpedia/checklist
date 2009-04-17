fb38fe9b051b05b8a85420ba02c7e8e615803872
cpedia <cpedia@23721618-7e43-11dd-88bd-cff828a63736>
2009-04-17 10:50:54 +0000

    quick checklist implementation.

--- chec.kli.st/checklist_web/static/jscripts/checklist.js
+++ chec.kli.st/checklist_web/static/jscripts/checklist.js
@@ -8,3 +8,23 @@ String.prototype.trim = function () {
     return this.replace(/^[\s\,]*/, "").replace(/[\s\,]*$/, ""); //for remove the space and comma at the begining/end of the tag.
 };
 
+var init_input_default_value = function init_input(id) {
+    var inp = document.getElementById(id).getElementsByTagName('input');
+    for (var i = 0; i < inp.length; i++) {
+        if (inp[i].type == 'text' || inp[i].type =="textarea") {
+            inp[i].setAttribute("rel", inp[i].defaultValue)
+            inp[i].onfocus = function() {
+                if (this.value == this.getAttribute("rel")) {
+                    this.value = "";
+                } else {
+                    this.select();
+                }
+            }
+            inp[i].onblur = function() {
+                if (this.value == "") {
+                    this.value = this.getAttribute("rel");
+                }
+            }
+        }
+    }
+};
