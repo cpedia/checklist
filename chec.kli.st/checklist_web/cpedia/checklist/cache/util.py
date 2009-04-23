8b35113f6b18070e79ce0fb0152c7c5b8f0e8b40
cpedia <cpedia@23721618-7e43-11dd-88bd-cff828a63736>
2009-04-23 03:46:24 +0000

    quick checklist implementation.

--- chec.kli.st/checklist_web/cpedia/checklist/cache/util.py
+++ chec.kli.st/checklist_web/cpedia/checklist/cache/util.py
@@ -41,36 +41,36 @@ def getUserChecklistPagination(user,page,checklist_num_per_page):
     key_ = "user_checklist"
     user_obj = getUser(user)
     count = user_obj.checklists_count
-    return models.UserChecklist.get_cached_page(key_,page,checklist_num_per_page,count,params=[user])
+    return models.UserChecklist.get_cached_page(key_,page,checklist_num_per_page,count,params=[user.user_id()])
 
 #get user's checklists pagination. Cached.
 def getUserStarredChecklistPagination(user,page,checklist_num_per_page):
     key_ = "user_starred_checklist"
     user_obj = getUser(user)
     count = user_obj.starred_checklists_count
-    return models.UserChecklist.get_cached_page(key_,page,checklist_num_per_page,count,params=[user])
+    return models.UserChecklist.get_cached_page(key_,page,checklist_num_per_page,count,params=[user.user_id()])
 
 #get user's checklists pagination. Cached.
 def getUserPublicChecklistPagination(user,page,checklist_num_per_page):
     key_ = "user_public_checklist"
     user_obj = getUser(user)
     count = user_obj.public_checklists_count
-    return models.UserChecklist.get_cached_page(key_,page,checklist_num_per_page,count,params=[user])
+    return models.UserChecklist.get_cached_page(key_,page,checklist_num_per_page,count,params=[user.user_id()])
 
 #get user's checklists pagination by Tag. Cached.
 def getUserTagChecklistPagination(user,tag,page,checklist_num_per_page):
     key_ = "user_tag_checklist"
     #todo: we assue the user will not have more than 1000 checklists for one tag, so we don't pass the count parameter.
-    return models.UserChecklist.get_cached_page(key_,page,checklist_num_per_page,params=[user,tag])
+    return models.UserChecklist.get_cached_page(key_,page,checklist_num_per_page,params=[user.user_id(),tag])
 
 def getUser(user_,nocache=False):
-    key_ = "user_"+user_.email()
+    key_ = "user_"+user_.user_id()
     try:
         user = memcache.get(key_)
     except Exception:
         user = None
     if nocache or user is None:
-        user = models.User.gql('WHERE user=:1',user_).get()
+        user = models.User.gql('WHERE user.user_id=:1',user_.user_id()).get()
         if user is None:
             usernew = models.User(user=user_)
             usernew.put()
@@ -83,13 +83,13 @@ def getUser(user_,nocache=False):
 
 def get_user_tags(user_,nocache=False):
     if user_ is not None:
-        key_ = "tag_"+user_.email()
+        key_ = "tag_"+user_.user_id()
         try:
             tags = memcache.get(key_)
         except Exception:
             tags = None
         if nocache or tags is None:
-            tags = models.Tag.gql('WHERE user=:1 ORDER BY entrycount desc',user_).fetch(1000)
+            tags = models.Tag.gql('WHERE user.user_id=:1 ORDER BY entrycount desc',user_.user_id()).fetch(1000)
             memcache.add(key=key_, value=tags)
         else:
             logging.debug("get_user_tags from cache.")
